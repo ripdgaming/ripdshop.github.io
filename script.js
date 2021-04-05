59837a506ee9dd56459b4ab33faa01a8b7cb67c3
ripdgaming <siddhantbilange@gmail.com>
2021-04-06 02:16:54 +0530

    publish

---
+++ script.js
@@ -0,0 +1,70 @@
+
+//_______________________Slideshow__________________________//
+var myIndex = 0;                       
+carousel();
+
+function carousel() 
+{
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+
+
+
+//_________________top scroll___________________//
+
+
+var mybutton = document.getElementById("myBtn");
+    
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() 
+{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+//________________LOGIN FORM______________//
+
+function myFunction() {
+  var x = document.getElementById("my-password");
+  var y = document.getElementById("hide-1");
+  var z = document.getElementById("hide-2");
+
+  if (x.type === "password") {
+    x.type = "text";
+    y.style.display = "block";
+    z.style.display = "none";
+  } else {
+    x.type = "password";
+    y.style.display = "none";
+    z.style.display = "block";
+  }
+}
+
+//________________GO TO BACK AS CLOSE______________//
+
+function goBack() {
+  window.history.back();
+}
+
